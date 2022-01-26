eac846fcb88a01c3c3c07d335e88aafa1178d142
benedettagns <benedetta.genise01@gmail.com>
2022-01-26 18:46:25 +0100

    title

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    h1 {
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+        font-style: oblique;
+        text-decoration: underline;
+        color: rgb(155, 7, 27);
+    }
+</style>
